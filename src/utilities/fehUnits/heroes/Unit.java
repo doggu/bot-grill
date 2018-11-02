26c7d892b5a393b345f2c8d3bb4ddf8fd4d27cdd
doggu <someoneyoumayknow0@gmail.com>
2018-11-01 18:57:07 -0700

    Unit constructor is now based on an actual Character object Implemented realistic IVs, and getters

--- src/utilities/fehUnits/heroes/Unit.java
+++ src/utilities/fehUnits/heroes/Unit.java
@@ -26,11 +26,12 @@ public class Unit extends Character {
      * creates a new unit based on a character;
      * generates IVs randomly
      *
-     * @param name - name of hero. must follow EXACT format:
+     * @param unit - name of hero. must follow EXACT format:
      *             "[name]: [epithet]"
+     * @param rarity - rarity of hero.
      */
-    public Unit(String name, int rarity) {
-        super(name);
+    public Unit(Character unit, int rarity) {
+        super(unit);
         this.rarity = rarity;
 
         int boon, bane;
@@ -38,78 +39,39 @@ public class Unit extends Character {
         int individuality = (int)(Math.random()*21);
         if (individuality==0) {
             //neutral
+            boon = -1;
+            bane = -1;
         } else {
             //damn this is fuckin retarded
-            switch (individuality/5) {
-                case 1:
-                    bane = 0; boon = 1; break;
-                case 2:
-                    bane = 2; boon = 1; break;
-                case 3:
-                    bane = 3; boon = 1; break;
-                case 4:
-                    bane = 4; boon = 1; break;
-                case 5:
-                    bane = 5; boon = 1; break;
-                case 6:
-                    bane = 0; boon = 2; break;
-                case 7:
-                    bane = 1; boon = 2; break;
-                case 8:
-                    bane = 3; boon = 2; break;
-                case 9:
-                    bane = 4; boon = 2; break;
-                case 10:
-                    bane = 5; boon = 2; break;
-                case 11:
-                    bane = 0; boon = 3; break;
-                case 12:
-                    bane = 1; boon = 3; break;
-                case 13:
-                    bane = 2; boon = 3; break;
-                case 14:
-                    bane = 4; boon = 3; break;
-                case 15:
-                    bane = 5; boon = 3; break;
-                case 16:
-                    bane = 0; boon = 4; break;
-                case 17:
-                    bane = 1; boon = 4; break;
-                case 18:
-                    bane = 2; boon = 4; break;
-                case 19:
-                    bane = 3; boon = 4; break;
-                case 20:
-                    bane = 5; boon = 4; break;
+            switch (individuality) {
+                case 1: boon = 0; bane = 1; break;
+                case 2: boon = 0; bane = 2; break;
+                case 3: boon = 0; bane = 3; break;
+                case 4: boon = 0; bane = 4; break;
+                case 5: boon = 1; bane = 0; break;
+                case 6: boon = 1; bane = 2; break;
+                case 7: boon = 1; bane = 3; break;
+                case 8: boon = 1; bane = 4; break;
+                case 9: boon = 2; bane = 0; break;
+                case 10: boon = 2; bane = 1; break;
+                case 11: boon = 2; bane = 3; break;
+                case 12: boon = 2; bane = 4; break;
+                case 13: boon = 3; bane = 0; break;
+                case 14: boon = 3; bane = 1; break;
+                case 15: boon = 3; bane = 2; break;
+                case 16: boon = 3; bane = 4; break;
+                case 17: boon = 4; bane = 0; break;
+                case 18: boon = 4; bane = 1; break;
+                case 19: boon = 4; bane = 2; break;
+                case 20: boon = 4; bane = 3; break;
                 default:
                     System.out.println("something hella broke");
                     throw new Error();
             }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        this.boon = 5;
-        this.bane = 3;
+        this.boon = boon;
+        this.bane = bane;
     }
 
     public int getBoon() { return boon; }

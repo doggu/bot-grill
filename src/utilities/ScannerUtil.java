ea7ac025c13d7b4b6216ed9ab0d4690ff02487db
doggu <someoneyoumayknow0@gmail.com>
2018-11-01 18:59:07 -0700

    Created ScannerUtil to help with the creation of file readers (might become deprecated once data can be fetched directly from https://feheroes.gamepedia.com)

---
+++ src/utilities/ScannerUtil.java
@@ -0,0 +1,21 @@
+package utilities;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ScannerUtil {
+    private static final boolean isWindows = System.getProperty("os.name").contains("Windows");
+
+    private static char getPathChar() {
+        if (isWindows) return '\\';
+        else return '/';
+    }
+
+    public static File createFile(String[] path) {
+        StringBuilder pathStr = new StringBuilder();
+        for (String x:path)
+            pathStr.append(x).append(getPathChar());
+        return new File(pathStr.toString());
+    }
+}

2e2238e79dc4e90f65284b2cef46a63776d08f34
doggu <someoneyoumayknow0@gmail.com>
2018-11-01 18:55:20 -0700

    Implemented ability to choose banner (very basic; needs fixing) Created attempt to print the release date of a banner

--- src/events/gameroom/SummonSimulator.java
+++ src/events/gameroom/SummonSimulator.java
@@ -1,9 +1,13 @@
 package events.gameroom;
 
+import com.sun.scenario.effect.Offset;
 import net.dv8tion.jda.core.entities.Message;
 import utilities.fehUnits.summoning.Banner;
 import utilities.fehUnits.summoning.BannerDatabase;
 
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class SummonSimulator extends Gameroom {
@@ -13,11 +17,32 @@ public class SummonSimulator extends Gameroom {
         //"rates: focus = " +[focus rate]+" 5* = "+[5* rate]
 
         List<Banner> list = BannerDatabase.getList();
-        Banner randomBanner = list.get((int)(Math.random()*list.size())); //temp
+        Banner banner = list.get((int)(Math.random()*list.size())); //temp
+
+        if (args.length>1) {
+            StringBuilder bannerName = new StringBuilder();
+            for (int i=1; i<args.length; i++) {
+                bannerName.append(args[i]);
+                if (i+1!=args.length) bannerName.append(" ");
+            }
+
+            for (Banner x:list) {
+                if (x.getName().equals(bannerName.toString())) {
+                    banner = x;
+                    break;
+                }
+            }
+        }
+
         e.getJDA().addEventListener(
-                new CircleSimulator(sendMessage("your summons for: "+randomBanner.getName()),
+                new CircleSimulator(
+                        sendMessage("your summons for: "+banner.getName()),
                         e.getAuthor(),
-                        randomBanner));
+                        banner));
+    }
+
+    private static String printDate(OffsetDateTime date) {
+        return date.getMonth()+"/"+date.getDayOfMonth()+"/"+date.getYear();
     }
 
     public boolean isCommand() {

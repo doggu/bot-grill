ed9a9ce4249c32d71f2718ef02ca21dc8d848849
doggu <someoneyoumayknow0@gmail.com>
2018-12-30 22:44:31 -0800

    created new structure

--- src/utilities/fehUnits/heroes/UnitDatabase.java
+++ src/utilities/fehUnits/heroes/UnitDatabase.java
@@ -7,7 +7,20 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class UnitDatabase {
-    public static final ArrayList<Hero> HEROES = adsgfnblhgfdsjlnkzbvfcjknxmljk;lnzfdjkgfds
+    public static final ArrayList<Hero> HEROES = getList();
+
+    private static ArrayList<Hero> getList() {
+        ArrayList<HeroConstructor> heroConstructors = new ArrayList<>();
+
+
+        //code goes here...
+
+
+        ArrayList<Hero> heroes = new ArrayList<>();
+        for (HeroConstructor x:heroConstructors)
+            heroes.add(x.createHero());
+        return heroes;
+    }
 
     public static void main(String[] args) {
 

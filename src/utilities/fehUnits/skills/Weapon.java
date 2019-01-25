f76daf5c87b3eb7232268ce9c61b16f3bfb4c45e
doggu <someoneyoumayknow0@gmail.com>
2019-01-24 17:54:15 -0800

    added weapon type (subject to change)

--- src/utilities/fehUnits/skills/Weapon.java
+++ src/utilities/fehUnits/skills/Weapon.java
@@ -2,17 +2,20 @@ package utilities.fehUnits.skills;
 
 public class Weapon extends Skill {
     private final int mt, rng;
+    private final String type;
 
     public Weapon(String name, String description, int cost, boolean exclusive,
-                  int mt, int rng) {
+                  int mt, int rng, String type) {
         super(name, description, 'W', cost, exclusive);
 
         this.mt = mt;
         this.rng = rng;
+        this.type = type;
     }
 
     public int getMt() { return mt; }
     public int getRng() { return rng; }
+    public String getType() { return type; }
 
     public String toString() {
         String info = name+"\n"

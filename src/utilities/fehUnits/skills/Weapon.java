58b1d7a69b72f73f48bb814562b7394dd6f5d91c
doggu <someoneyoumayknow0@gmail.com>
2019-01-24 17:56:23 -0800

    modified toString to include weapon type

--- src/utilities/fehUnits/skills/Weapon.java
+++ src/utilities/fehUnits/skills/Weapon.java
@@ -20,36 +20,7 @@ public class Weapon extends Skill {
     public String toString() {
         String info = name+"\n"
                 + "```\n"
-                + "Type: ";
-        String skillType;
-        switch(slot) {
-            //TODO: should differentiate between weapon types later
-            case 0:
-                skillType = "Weapon";
-                break;
-            case 1:
-                skillType = "Assist";
-                break;
-            case 2:
-                skillType = "Special";
-                break;
-            case 3:
-                skillType = "A Passive";
-                break;
-            case 4:
-                skillType = "B Passive";
-                break;
-            case 5:
-                skillType = "C Passive";
-                break;
-            case 6:
-                skillType = "Sacred Seal";
-                break;
-            default:
-                System.out.println("this literally shouldn't happen");
-                throw new Error();
-        }
-        info+= skillType+"\n"
+                + "Type: "+type+"\n"
                 + "Range: "+rng+"\n"
                 + "Cost: "+cost+"\n"
                 + "Exclusive: "+(exclusive?"Yes":"No")+"\n\n"

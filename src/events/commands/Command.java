a968963634e2a14c3516a686deb496c57e689455
doggu <someoneyoumayknow0@gmail.com>
2019-02-04 23:38:19 -0800

    this should be done soon (before things get too big)

--- src/events/commands/Command.java
+++ src/events/commands/Command.java
@@ -4,6 +4,8 @@ import events.MessageListener;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 
 //wtf did i even make this for
+//TODO: make something other than this for more diverse situations
+// this shit is dominating pretty much every listener
 public abstract class Command extends MessageListener {
     public abstract boolean isCommand();
     protected char getPrefix() { return '?'; }

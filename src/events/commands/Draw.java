bf3786886bfa5bb1cee0532b83a55876ab8c3cac
doggu <someoneyoumayknow0@gmail.com>
2019-02-09 18:28:41 -0800

    improved organization (even though this is just a test)

--- src/events/commands/Draw.java
+++ src/events/commands/Draw.java
@@ -2,15 +2,38 @@ package events.commands;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
-import java.awt.image.BufferedImageOp;
-import java.awt.image.ColorConvertOp;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Draw extends Command {
+    private static final int WIDTH = 6, HEIGHT = 8, SCALE = 64;
+    private static final ArrayList<BufferedImage> heroFaces = getFaces();
+    private static ArrayList<BufferedImage> getFaces() {
+        File folder = new File("./libs/heroes");
+        ArrayList<BufferedImage> heroFaces = getImagesFromFolder(folder);
+        return heroFaces;
+    }
+    //TODO: the function below can probably be lambda'd or something
+    private static ArrayList<BufferedImage> getImagesFromFolder(File folder) {
+        ArrayList<BufferedImage> images = new ArrayList<>();
+        File[] fileNames = folder.listFiles();
+        for (File x:fileNames) {
+            try {
+                images.add(ImageIO.read(x));
+            } catch (IOException notAnImageIGuess) {
+                System.out.println("ran into a file that isn't an image: "+x.getName());
+                //TODO: make recursive? probably not
+            }
+        }
+        return images;
+    }
+
+
+
     @Override
     public boolean isCommand() {
         return args[0].equalsIgnoreCase("Draw");
@@ -18,22 +41,11 @@ public class Draw extends Command {
 
     public void onCommand() {
         //test for drawing maps
-        int WIDTH = 6, HEIGHT = 8, SCALE = 113;
         int width = WIDTH*SCALE;
         int height = HEIGHT*SCALE;
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = bufferedImage.createGraphics();
 
-        ArrayList<BufferedImage> heroFace = new ArrayList<>();
-        File folder = new File("./libs/heroes");
-        File[] fileNames = folder.listFiles();
-        for (File x:fileNames) {
-            try {
-                heroFace.add(ImageIO.read(x));
-            } catch (IOException notAnImageIGuess) {
-                System.out.println("ran into a file that isn't an image: "+x.getName());
-            }
-        }
 
 
         ArrayList<Integer> previousX = new ArrayList<>();
@@ -47,7 +59,16 @@ public class Draw extends Command {
             previousX.add(x);
             previousY.add(y);
 
-            g2d.drawImage(heroFace.get((int)(Math.random()*heroFace.size())), null, x, y);
+            //TODO: make this less stupid when i'm less stupid
+            BufferedImage face = heroFaces.get((int)(Math.random()*heroFaces.size()));
+            double scale = 64.0/face.getHeight();
+            g2d.drawImage(
+                    face,
+                    //there must be a better way to do this (or not)
+                    new AffineTransformOp(
+                            new AffineTransform(scale,0,0, scale,0 ,0),
+                            AffineTransformOp.TYPE_BICUBIC),
+                    x, y);
         }
 
         // Disposes of this graphics context and releases any system resources that it is using.

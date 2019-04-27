f482e6d569dd33287f4ce0129e75182b0d42596e
doggu <someoneyoumayknow0@gmail.com>
2019-04-26 21:51:03 -0700

    outlined

--- src/utilities/feh/battle/Battle.java
+++ src/utilities/feh/battle/Battle.java
@@ -1,5 +1,29 @@
 package utilities.feh.battle;
 
+import events.gameroom.mapTest.Map;
+
+import java.util.ArrayList;
+
 public class Battle {
+    private final int turn;
+    private final int turnLimit;
+    private final Map map;
+    private final ArrayList<FieldedUnit> team1, team2;
+
+
+    public Battle(ArrayList<FieldedUnit> team1, ArrayList<FieldedUnit> team2, Map map) {
+        this.team1 = team1;
+        this.team2 = team2;
+        this.map = map;
+
+        this.turn = 1;
+        this.turnLimit = Integer.MAX_VALUE;
+    }
+
+
+
+    public boolean isOddTurn() { return turn%2==1; }
+    public boolean isEvenTurn() { return !isOddTurn(); }
+
 
 }

95a6bb8be0ec363d16595d9578045b66bb5faf7d
doggu <someoneyoumayknow0@gmail.com>
2019-08-10 02:09:58 -0700

    increased flexibility of button actions

--- src/events/ReactionButton.java
+++ src/events/ReactionButton.java
@@ -1,38 +1,47 @@
 package events;
 
-import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Emote;
 import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.events.message.react.MessageReactionAddEvent;
 
-import static main.BotMain.bot_grill;
-
-public class ReactionButton extends ReactionListener {
+public abstract class ReactionButton extends ReactionListener {
     private final Message message;
     private final Emote emote;
-    private final MessageBuilder response;
 
-    public ReactionButton(Message message, Emote emote, MessageBuilder response) {
+    protected ReactionButton(Message message, Emote emote) {
         this.message = message;
         this.emote = emote;
-        this.response = response;
 
         message.addReaction(emote).complete();
-
-        bot_grill.addEventListener(this);
     }
 
 
+
+    public Message getMessage() { return message; }
+
+
+
+    public void onMessageReactionAdd(MessageReactionAddEvent event) {
+        this.e = event;
+
+        if (isCommand()) {
+            onCommand();
+            commitSuicide();
+        }
+    }
 
     public boolean isCommand() {
         if (e.getUser().isBot()) return false;
         if (!e.getMessageId().equals(message.getId())) return false;
+        /*
         if (!e.getReactionEmote().getEmote().getId().equals(emote.getId())) return false;
 
         return true;
+
+         *///just because i lov u jetbreins and want to see u check mark
+
+        return e.getReactionEmote().getEmote().getId().equals(emote.getId());
     }
 
-    public void onCommand() {
-        sendMessage(response.build());
-        e.getJDA().removeEventListener(this);
-    }
+    public abstract void onCommand();
 }

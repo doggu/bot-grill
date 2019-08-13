5c8c861990d6a5551f4ba7e7d7aa0f6fdcac68b7
doggu <someoneyoumayknow0@gmail.com>
2019-08-13 11:47:16 -0700

    removed deprecated code and added "promises" for the future

--- src/feh/FEHeroesCache.java
+++ src/feh/FEHeroesCache.java
@@ -14,6 +14,7 @@ public class FEHeroesCache extends WebCache {
 
 
 
+    //TODO: replace old utilities with new jsoupy ones
     public FEHeroesCache(String wiki, String subdir) {
         super(FEHEROES_URL+wiki, FEHEROES_SUBDIR+(subdir!=null?subdir:""));
     }
@@ -54,48 +55,6 @@ public class FEHeroesCache extends WebCache {
         return data;
     }
 
-    /**
-     * this one gets a description since it's unclear
-     * @param tableFormat the exact form, in plain text, of the table html tag to look for
-     * @return the data of the table in a single, linear array (you gotta figure out where the rows end lol)
-     */
-    public ArrayList<String> getTable(String tableFormat) {
-        ArrayList<String> table = new ArrayList<>();
-
-        Scanner file;
-        try {
-            file = new Scanner(this);
-        } catch (FileNotFoundException fnfe) {
-            throw new Error("could not find file "+getName());
-        }
-        String line;
-        while (file.hasNextLine()) {
-            line = file.nextLine();
-            if (line.contains(tableFormat))
-                table = getItems(line.chars());
-        }
-
-        return table;
-    }
-
-    //for BannerDatabase
-    public String getLongAssLine(int threshold) {
-        Scanner input;
-        try {
-            input = new Scanner(this);
-        } catch (FileNotFoundException fnfe) {
-            throw new Error("could not find file at "+getPath());
-        }
-
-        while (input.hasNextLine()) {
-            String line = input.nextLine();
-            if (line.length()>threshold)
-                return line;
-        }
-
-        return null;
-    }
-
     private static ArrayList<String> getItems(IntStream source) {
         ArrayList<String> data = new ArrayList<>();
         int[] chars = source.toArray();
@@ -119,23 +78,5 @@ public class FEHeroesCache extends WebCache {
         }
 
         return data;
-    }
-
-
-
-    public static void main(String[] args) {
-        FEHeroesCache test = new FEHeroesCache("Weapon_Refinery");
-        ArrayList<String> table =
-                test.getTable("<table style=\"" +
-                        "display:inline-table;" +
-                        "border:1px solid #a2a9b1;" +
-                        "border-collapse:collapse;" +
-                        "width:24em;" +
-                        "margin:0.5em 0;" +
-                        "background-color:#f8f9fa\">");
-
-        for (String datum:table) {
-            System.out.println(datum.trim());
-        }
     }
 }

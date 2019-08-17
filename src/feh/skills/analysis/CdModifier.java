9070604c3a08e891cba73738519cdaf595379c3b
doggu <someoneyoumayknow0@gmail.com>
2019-08-17 02:30:12 -0700

    removed unnecessary class

--- src/feh/skills/analysis/CdModifier.java
+++
@@ -1,6 +0,0 @@
-package feh.skills.analysis;
-
-//only for hard cooldown modifiers (Slaying Edge, Lightning Breath, etc.)
-// not accelerators/slowers (Flashing Blade, Special Spiral, Wrath, etc.)
-public class CdModifier implements SkillProperty {
-}

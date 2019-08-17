f09aca6ba81180ff9c0eccafbe028aa7edc44249
doggu <someoneyoumayknow0@gmail.com>
2019-08-17 02:31:09 -0700

    created more functionality stuff (one day i swear)

--- src/feh/skills/skillTypes/Skill.java
+++ src/feh/skills/skillTypes/Skill.java
@@ -1,7 +1,15 @@
 package feh.skills.skillTypes;
 
+import feh.skills.SkillDatabase;
+import feh.skills.analysis.CooldownModifier;
+import feh.skills.analysis.StatModifier;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public abstract class Skill {
     final String name, description;
+    private final String[] args;
 
     //0 = weapon, 1 = assist, 2 = special,
     //3 = a passive, 4 = b passive, 5 = c passive, 6 = seal
@@ -31,6 +39,23 @@ public abstract class Skill {
         }
         this.cost = cost;
         this.exclusive = exclusive;
+
+
+
+        //remove last period and split up by sentences
+        try {
+            if (description.length()==0) // stoopit silve r swor d
+                args = new String[0];
+            else
+                args = description.substring(0, description.length() - 1).split("\\. ");
+        } catch (IndexOutOfBoundsException ioobe) {
+            ioobe.printStackTrace();
+            throw new Error(description);
+        }
+
+
+
+        generateProperties();
     }
 
 
@@ -41,10 +66,24 @@ public abstract class Skill {
     public int getSlot() { return slot; }
     public boolean isExclusive() { return exclusive; }
 
+    private void generateProperties() {
+        //System.out.println(name);
+        //for (String x:args) System.out.println(x);
+        //System.out.println("\n\n");
+
+        if (this instanceof StatModifier) {
+
+        }
+
+        if (this instanceof CooldownModifier) {
+
+        }
+    }
+
 
 
     public String toString() {
-
+        /*
         String info = name+"\n"
                 + "```\n"
                 + "Type: ";
@@ -73,15 +112,20 @@ public abstract class Skill {
                 skillType = "Sacred Seal";
                 break;
             default:
-                System.out.println("this literally shouldn't happen");
-                throw new Error();
+                throw new Error("this literally shouldn't happen");
         }
         info+= skillType+"\n"
                 + "Cost: "+cost+"\n"
                 + "Exclusive: "+(exclusive?"Yes":"No")+"\n\n"
                 + description+"\n"
                 + "```";
-
+         */
         return name;
+    }
+
+
+
+    public static void main(String[] args) {
+        ArrayList<Skill> skills = SkillDatabase.SKILLS;
     }
 }

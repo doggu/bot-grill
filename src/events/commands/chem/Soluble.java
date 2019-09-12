31e1039a58189fb424dc1fc630edc1b9e55b32aa
doggu <someoneyoumayknow0@gmail.com>
2019-09-11 22:12:23 -0500

    lemme put my todos here: think about molecules and what ions are

---
+++ src/events/commands/chem/Soluble.java
@@ -0,0 +1,33 @@
+package events.commands.chem;
+
+import events.commands.Command;
+
+public class Soluble extends Command {
+
+    @Override
+    public void onCommand() {
+        if (args.length!=6) {
+            sendMessage("please provide two compounds you wish to compare.");
+        }
+    }
+
+    @Override
+    public boolean isCommand() {
+        return args[0].toLowerCase().matches("s(oluble)?");
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public String getDescription() {
+        return null;
+    }
+
+    @Override
+    public String getFullDescription() {
+        return null;
+    }
+}

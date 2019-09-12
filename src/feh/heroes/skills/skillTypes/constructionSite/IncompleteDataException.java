db2c7d6d95f6e96fb37b616e2fd62477ea540841
doggu <someoneyoumayknow0@gmail.com>
2019-09-12 02:57:08 -0500

    no yellows (not racist)

--- src/feh/heroes/skills/skillTypes/constructionSite/IncompleteDataException.java
+++ src/feh/heroes/skills/skillTypes/constructionSite/IncompleteDataException.java
@@ -1,6 +1,6 @@
 package feh.heroes.skills.skillTypes.constructionSite;
 
-public class IncompleteDataException extends Exception {
+class IncompleteDataException extends Exception {
     IncompleteDataException(String field) {
         super(field);
     }

2d262caa26e2aeeecc51d750797ad152394a7fda
doggu <someoneyoumayknow0@gmail.com>
2019-09-20 02:30:28 -0500

    ヽຈل͜ﾉ༼ຈ

--- src/events/commands/chem/ElementRetriever.java
+++ src/events/commands/chem/ElementRetriever.java
@@ -7,6 +7,42 @@ import stem.science.chem.ElementDatabase;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+/*
+
++--+                                               +--+
+|H |                                               |He|
++--+--+                             +--+--+--+--+--+--+
+|Li|Be|                             |B |C |N |O |F |Ne|
++--+--+                             +--+--+--+--+--+--+
+|Na|Mg|                             |Al|Si|P |S |Cl|Ar|
++--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+|K |Ca|Sc|Tl|V |Cr|Mn|Fe|Co|Ni|Cu|Zn|Ga|Ge|As|Se|Br|Kr|
++--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+|Rb|Sr|Y |Zr|Nb|Mo|Tc|Ru|Rh|Pd|Ag|Cd|In|Sn|Sb|Te|I |Xe|
++--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+|Cs|Ba|La|Hf|Ta|W |Re|Os|Ir|Pt|Au|Hg|Tl|Pb|Bi|Po|At|Rn|
++--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+|Fr|Ra|Ac|Rf|Ha|  |  |  |  |
++--+--+--+--+--+--+--+--+--+
+
++--+                                                                                         +--+
+|H |                                                                                         |He|
++--+--+                                                                       +--+--+--+--+--+--+
+|Li|Be|                                                                       |B |C |N |O |F |Ne|
++--+--+                                                                       +--+--+--+--+--+--+
+|Na|Mg|                                                                       |Al|Si|P |S |Cl|Ar|
++--+--+--+                                         +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+|K |Ca|Sc|                                         |Tl|V |Cr|Mn|Fe|Co|Ni|Cu|Zn|Ga|Ge|As|Se|Br|Kr|
++--+--+--+                                         +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+|Rb|Sr|Y |                                         |Zr|Nb|Mo|Tc|Ru|Rh|Pd|Ag|Cd|In|Sn|Sb|Te|I |Xe|
++--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+|Cs|Ba|La|Ce|Pr|Nd|Pm|Sm|Eu|Gd|Tb|Dy|Ho|Er|Tm|Yb|Lu|Hf|Ta|W |Re|Os|Ir|Pt|Au|Hg|Tl|Pb|Bi|Po|At|Rn|
++--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+|Fr|Ra|Ac|Th|Pa|U |Np|Pu|Am|Cm|Bk|Cf|Es|Fm|Md|No|Lr|Rf|Ha|  |  |  |  |
++--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+ */
+
 public class ElementRetriever extends Command {
     private final ArrayList<ChemicalElement> elementList;
 

f5c44ed4491f86a0c4a42bcba997102529656594
doggu <someoneyoumayknow0@gmail.com>
2019-09-20 07:07:13 -0500

    i can't even copy good

---
+++ src/feh/heroes/skills/analysis/Effect.java
@@ -0,0 +1,11 @@
+package feh.heroes.skills.analysis;
+
+import feh.battle.FieldedUnit;
+
+import java.util.function.Function;
+
+public class Effect {
+    //phase/situation already met as a result of storage method
+    private Function<FieldedUnit, Boolean> condition;
+    //private BFunction<Battle, >
+}

ba2db69b56a89cbad03bee7390e9a19d834afb4c
doggu <someoneyoumayknow0@gmail.com>
2019-09-23 01:49:57 -0500

    this should probably be part of barracks

--- src/feh/players/relationships/Relationship.java
+++ src/feh/players/relationships/Relationship.java
@@ -1,15 +1,30 @@
 package feh.players.relationships;
 
+import feh.battle.Battle;
 import feh.heroes.character.Hero;
+
+import java.util.ArrayList;
 
 public class Relationship {
     protected final Hero h1, h2;
-    private int maps = 0;
+    private ArrayList<Battle>
+            maps = new ArrayList<>(); //a bit excessive but at least it's comprehensive and foolproof (for myself)
 
     public Relationship(Hero h1, Hero h2) {
         //if (h1==h2) throw new CannotBangSelfException(); being handled in Relationships
         this.h1 = h1;
         this.h2 = h2;
+    }
+
+
+
+    public boolean addMap(Battle b) {
+        if (!maps.contains(b)) {
+            maps.add(b);
+            return true;
+        }
+
+        return false;
     }
 
 

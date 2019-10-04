908b6be13ca2ade4b479cfd594942c1b5a7fd1e6
doggu <someoneyoumayknow0@gmail.com>
2019-10-04 06:43:07 -0500

    really not sure where to go with this

---
+++ src/feh/heroes/character/LegendaryHero.java
@@ -0,0 +1,13 @@
+package feh.heroes.character;
+
+import feh.heroes.skills.skillTypes.Skill;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+public class LegendaryHero extends Hero {
+    public LegendaryHero(HeroName fullName, Origin origin, URL gamepediaLink, URL portraitLink, String artist, char gender, char color, WeaponClass weaponType, MovementClass moveType, int summonableRarity, Availability availability, GregorianCalendar dateReleased, HeroStats stats, ArrayList<Skill> baseKit) {
+        super(fullName, origin, gamepediaLink, portraitLink, artist, gender, color, weaponType, moveType, summonableRarity, availability, dateReleased, stats, baseKit);
+    }
+}

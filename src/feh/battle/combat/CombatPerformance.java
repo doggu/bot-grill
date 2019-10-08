5def46aea761a802e7fd055793305b716e616d96
doggu <someoneyoumayknow0@gmail.com>
2019-10-08 04:17:47 -0500

    created to organize/separate internal combat statistics from flow

---
+++ src/feh/battle/combat/CombatPerformance.java
@@ -0,0 +1,17 @@
+package feh.battle.combat;
+
+public class CombatPerformance {
+    protected boolean didAttack;
+    protected boolean triggeredSpecial;
+
+
+
+    CombatPerformance() {
+
+    }
+
+
+
+
+
+}

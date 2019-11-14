92762605a76cf39ffb5d8caf4b84cae938194101
doggu <someoneyoumayknow0@gmail.com>
2019-11-14 00:33:46 -0600

    :retreat:

--- src/events/devTools/DevTools.java
+++ src/events/devTools/DevTools.java
@@ -1,8 +1,12 @@
 package events.devTools;
 
 import events.MessageListener;
+import net.dv8tion.jda.core.entities.VoiceChannel;
+import net.dv8tion.jda.core.managers.AudioManager;
 
 public class DevTools extends MessageListener {
+    private static AudioManager am = null;
+
     public void onCommand() {
         System.out.println(e.getMessage().getContentRaw());
         switch(args[0].toLowerCase()) {
@@ -12,12 +16,26 @@ public class DevTools extends MessageListener {
                 break;
             case "sendmessage":
                 StringBuilder message = new StringBuilder();
-                for (int i=1; i<args.length; i++) {
+                for (int i = 1; i<args.length; i++) {
                     message.append(args[i]).append(" ");
                 }
 
                 sendMessage(message.toString());
                 break;
+            case "invade":
+                VoiceChannel channel = e.getGuild()
+                        .getVoiceChannelsByName("musak", true).get(0);
+                am = e.getGuild().getAudioManager();
+
+                am.openAudioConnection(channel);
+                break;
+            case "retreat":
+                if (am==null) {
+                    sendMessage("i haven't invaded yet.");
+                } else {
+                    am.closeAudioConnection();
+                    am = null;
+                }
         }
     }
 

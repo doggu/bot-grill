fb9fc74b6d217358896c7ca45a7f15331b7462ec
doggu <someoneyoumayknow0@gmail.com>
2019-11-14 18:08:45 -0600

    added many markdown and other discord formatting features

--- src/discordUI/DiscordSB.java
+++ src/discordUI/DiscordSB.java
@@ -1,14 +1,32 @@
 package discordUI;
 
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.Message;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.entities.User;
+import org.jetbrains.annotations.NotNull;
 
-//@SuppressWarnings("all")
+import javax.annotation.Nullable;
+
+@SuppressWarnings("unused")
 public class DiscordSB extends MessageBuilder {
-    public DiscordSB(CharSequence str) {
-        super(str);
+    public DiscordSB() {}
+    public DiscordSB(CharSequence content) {
+        super(content);
     }
-
+    public DiscordSB(Message message) {
+        super(message);
+    }
+    public DiscordSB(MessageBuilder builder) {
+        super(builder);
+    }
+    public DiscordSB(EmbedBuilder builder) {
+        super(builder);
+    }
+    public DiscordSB(MessageEmbed embed) {
+        super(embed);
+    }
 
 
     //idk if this is sacreligious or something
@@ -18,16 +36,64 @@ public class DiscordSB extends MessageBuilder {
     }
 
 
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                    MARKDOWN                                                    //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //                                                   CODE STUFF                                                   //
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    public DiscordSB wrapCode() {
-        builder.insert(0, '`').append('`');
-        return this;
+    //general formatting
+    /**
+     * uses generally normal boxing shit to italicize a certain range in the StringBuilder.
+     * @param start the first character to be italicizec
+     * @param end the character after the last character to be italicized
+     * @return this object.
+     */
+    public DiscordSB italicize(int start, int end) {
+        return wrap(start, end, '*');
     }
+    public DiscordSB italicize() {
+        return italicize(0, builder.length());
+    }
+
+    public DiscordSB bold(int start, int end) {
+        return wrap(start, end, "**");
+    }
+    public DiscordSB bold() {
+        return bold(0, builder.length());
+    }
+
+    public DiscordSB boltalicize(int start, int end) {
+        return wrap(start, end, "***");
+    }
+    public DiscordSB boltalicize() {
+        return boltalicize(0, builder.length());
+    }
+
+    public DiscordSB underline(int start, int end) {
+        return wrap(start, end, "__");
+    }
+    public DiscordSB underline() {
+        return underline(0, builder.length());
+    }
+
+    public DiscordSB strikethrough(int start, int end) {
+        return wrap(start, end, "~~");
+    }
+    public DiscordSB strikethrough() {
+        return strikethrough(0, builder.length());
+    }
+
+
+    //code lines/blocks
+    public DiscordSB wrapCode() {
+        return wrap (0, builder.length(), '`');
+    }
+    public DiscordSB wrapCode(int start, int end) {
+        return wrap(start, end, '`');
+    }
+
+    @Nullable
     public DiscordSB blockCode(String language) {
-        builder.insert(0, "```"+language).append("```");
+        builder.insert(0, "```" + language).append("```");
         return this;
     }
 
@@ -35,17 +101,49 @@ public class DiscordSB extends MessageBuilder {
         builder.append('`').append(code).append('`');
         return this;
     }
-    public DiscordSB appendCodeBlock(String language, String code) {
-        builder.append("```").append(language).append('\n').append(code).append("```");
+    @Nullable
+    public DiscordSB appendCodeBlock(@Nullable String language, String code) {
+        builder.append("```");
+        if (language!=null) builder.append(language);
+        builder.append('\n').append(code).append("```");
         return this;
     }
 
-    public DiscordSB mention(StringBuilder s, User user) {
+
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //                                                       UI                                                       //
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public DiscordSB mention(@NotNull User user) {
         super.append(user.getAsMention());
         return this;
     }
+    //implementing an insertion version is probably unnecessary
 
 
+    //spoilers
+    public DiscordSB wrapSpoiler(int start, int end) {
+        return wrap(start, end, "||");
+    }
+
+    //todo: maybe use id enum to abstractify append
+    public DiscordSB appendSpoiler(String spoiler) {
+        builder.append("||").append(spoiler).append("||");
+        return this;
+    }
+
+
+    //private utilities that seemed useful enough to be public
+
+    public DiscordSB wrap(int start, int end, char c) {
+        builder.insert(start, c).insert(end, c);
+        return this;
+    }
+
+    public DiscordSB wrap(int start, int end, String s) {
+        builder.insert(start, s).insert(end, s);
+        return this;
+    }
 
     /* shit i wrote when i was going to completely remake StringBuilder
 

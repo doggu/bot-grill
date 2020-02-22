0426a4ab3585be6f4d3c5eb3326507e3ac208133
doggu <someoneyoumayknow0@gmail.com>
2020-02-22 02:53:30 -0600

    fixx

--- src/events/commands/campus/CanIEatRightNow.java
+++ src/events/commands/campus/CanIEatRightNow.java
@@ -32,18 +32,20 @@ public class CanIEatRightNow extends Command {
 
         switch (e.getAuthor().getId()) {
             case "125857288807251968": //me
-                if (Schedule.PIONEER_HALL.canGo(TimeZone.getTimeZone("GMT-5"), hours)) {
+                if (Schedule.PIONEER_HALL.canGo(TimeZone.getTimeZone("GMT-6"), hours)) {
                     sendMessage("yes!");
+                } else {
+                    sendMessage("no.");
                 }
                 break;
             case "235275443941539840": //da-CoUGH i mean Satan
-                if (Schedule.DLG_COMMONS.canGo(TimeZone.getTimeZone("GMT-7"), hours)) {
-                    if (Schedule.CARRILLO_COMMONS.canGo(TimeZone.getTimeZone("GMT-7"), hours)) {
+                if (Schedule.DLG_COMMONS.canGo(TimeZone.getTimeZone("GMT-8"), hours)) {
+                    if (Schedule.CARRILLO_COMMONS.canGo(TimeZone.getTimeZone("GMT-8"), hours)) {
                         sendMessage("yes! both Carrillo and DLG "+(hours==0?"are":(hours>0?"will be":"were"))+" open.");
                     } else {
                         sendMessage("yes! DLG "+(hours==0?"is":(hours>0?"will be":"was"))+" open.");
                     }
-                } else if (Schedule.CARRILLO_COMMONS.canGo(TimeZone.getTimeZone("GMT-7"), hours)) {
+                } else if (Schedule.CARRILLO_COMMONS.canGo(TimeZone.getTimeZone("GMT-8"), hours)) {
                     sendMessage("yes! Carrillo "+(hours==0?"is":(hours>0?"will be":"was"))+" open.");
                 } else {
                     sendMessage("sorry, neither DLG nor Carrillo "+

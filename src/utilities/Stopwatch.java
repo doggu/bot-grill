ef121e1c26ae8a1bfd145387429773bed8bc9135
doggu <someoneyoumayknow0@gmail.com>
2020-04-14 12:12:06 -0700

    small bug which invalidated an entire component of the class

--- src/utilities/Stopwatch.java
+++ src/utilities/Stopwatch.java
@@ -6,17 +6,10 @@ import java.math.MathContext;
 public class Stopwatch {
     private long start = 0, stop = 0;
 
-    public Stopwatch() {}
 
     public void start() { start = System.nanoTime(); }
     public void stop() { stop = System.nanoTime(); }
     private long nanosecondsElapsed() { return stop-start; }
-    public long timeInMilliseconds() {
-        if (start==0) return 0L;
-        if (stop==0) stop();
-
-        return nanosecondsElapsed()/1000000;
-    }
 
     public double timeInSeconds() {
         if (start==0) return 0L;
@@ -35,8 +28,10 @@ public class Stopwatch {
         return "done ("+BigDecimal.valueOf(timeInSeconds()).round(new MathContext(3))+")!";
     }
 
-
     public double split() {
-        return (start-stop)/1000000000.0;
+        long stop = nanosecondsElapsed();
+        this.stop = 0;
+        return BigDecimal.valueOf(stop / 1000000000.0)
+                .round(new MathContext(3)).doubleValue();
     }
 }

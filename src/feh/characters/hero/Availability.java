b55c66253bfd1070dd1e8e9fa6867b93ba8659d1
doggu <someoneyoumayknow0@gmail.com>
2020-06-04 02:23:49 -0700

    prospective new format for summoning info

--- src/feh/characters/hero/Availability.java
+++ src/feh/characters/hero/Availability.java
@@ -1,3 +1,58 @@
+/* future code
+package feh.characters.hero;
+
+import java.util.Date;
+
+public class Availability {
+public enum Type {
+    NORMAL(true, true),
+//        NORMAL_RARITY_CHANGED(true, true),
+    SEASONAL(true, false),
+    LEGENDARY(true, false),
+    MYTHIC(true, false),
+    STORY(false, false),
+//        ENEMY(false, false),
+    GHB(false, false),
+    TT(false, false);
+
+    private final boolean
+            isSummonable,
+            isInNormalPool;
+
+
+    Type(boolean isSummonable, boolean isInNormalPool) {
+        this.isSummonable = isSummonable;
+        this.isInNormalPool = isInNormalPool;
+    }
+
+    public boolean isSummonable() { return isSummonable; }
+    public boolean isInNormalPool() { return isInNormalPool; }
+}
+
+
+
+/**
+ * determines whether or not the summonability of a unit represented by this
+ * coincides in any way with the provided date
+ *
+ * @param date the date to compare against this's availability
+ * @return an integer indicating star rating in the random pool
+ *             3 representing 3-4*,
+ *             4 representing 4-5*,
+ *             5 representing 5* availability,
+ *             -1 representing the unit not being available in the random
+ *             pool.
+ *
+public int rarityInRandomPool(Date date) {
+//compare to this unit's date ranges and
+return -1;
+}
+
+public int rarityInRandomPool() {
+    return rarityInRandomPool(new Date()); }
+}
+*/
+
 package feh.characters.hero;
 
 public enum Availability {
@@ -25,4 +80,6 @@ public enum Availability {
 
     public boolean isSummonable() { return isSummonable; }
     public boolean isInNormalPool() { return isInNormalPool; }
+
+
 }
